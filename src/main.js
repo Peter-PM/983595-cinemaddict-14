8f7a4c362cec345c32415d2568884478c7c11032
Peter-PM <tangompp@gmail.com>
2021-03-31 01:32:18 +0300

    added modules

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import {createSiteMenuTemplate} from './view/site-menu.js';
+import {createSiteUserRatingTemplate} from './view/user-rating.js';
+import {createFilmListTemplate} from './view/film-list.js';
+import {createFilmCardTemplate} from './view/film-card.js';
+import {createShowMoreButtonTemplate} from './view/show-more.js';
+import {createFilmPopupTemplate} from './view/film-details-popup.js';
+import {createFooterStatisticTemplate} from './view/footer-statistic.js';
+import {createFilmListTopTemplate} from './view/film-list-top.js';
+import {createFilmListCommentTemplate} from './view/film-list-comment.js';
+
+const TASK_COUNT = 5;
+
+const render = (container, template, place = 'beforeend') => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector('.main');
+const siteHeaderLogoElement = document.querySelector('.header__logo');
+const siteFooterStatisticElement = document.querySelector('.footer__statistics');
+
+render(siteHeaderLogoElement, createSiteUserRatingTemplate());
+render(siteMainElement, createSiteMenuTemplate());
+render(siteMainElement, createFilmListTemplate());
+
+const filmSection = document.querySelector('.films');
+const filmList = document.querySelector('.films-list');
+const filmListContainer = document.querySelector('.films-list__container');
+
+for (let i = 0; i < TASK_COUNT; i++) {
+  render(filmListContainer, createFilmCardTemplate());
+};
+
+render(filmList, createShowMoreButtonTemplate());
+//render(siteMainElement, createFilmPopupTemplate());
+
+render(filmSection, createFilmListTopTemplate());
+render(filmSection, createFilmListCommentTemplate());
+
+const filmsList = document.querySelectorAll('.films-list--extra');
+
+console.log(filmsList)
+filmsList.forEach((el) => {
+  for (let i = 0; i < 2; i++) {
+    let cont = el.querySelector('.films-list__container');
+    render(cont, createFilmCardTemplate());
+  };
+})
+
+render(siteFooterStatisticElement, createFooterStatisticTemplate());
---
+++ src/view/film-list.js
@@ -0,0 +1,8 @@
+export const createFilmListTemplate = () => {
+  return `<section class="films">
+  <section class="films-list">
+    <h2 class="films-list__title">There are no movies in our database</h2>
+    <div class="films-list__container"></div>
+  </section>
+</section>`;
+};
